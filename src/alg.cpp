ef23bfb465d6feea6943e137653894b421deb771
SolovevNik22IST1 <125061447+SolovevNik22IST1@users.noreply.github.com>
2023-04-09 01:55:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,90 +3,89 @@
 #include <map>
 #include "tstack.h"
 
-int priority(char x) {
-  if (x == '(') return 0;
-  else if (x == ')') return 1;
-  else if (x == '+' || x == '-') return 2;
-  else if (x == '*' || x == '/') return 3;
-  else 
-    return -1;
+int num(char x) {
+    return (x >= '0' && x <= '9');
 }
 
-int operations(char x) {
-  return (x == '+' || x == '-' \
-          || x == '*' || x == '/');
+int op(char x) {
+    return (x == '+' || x == '-' \
+            || x == '*' || x == '/');
 }
 
-int number(char x) {
-  return (x >= '0' && x <= '9');
+int pr(char x) {
+    if (x == '(') return 0;
+    else if (x == ')') return 1;
+    else if (x == '+' || x == '-') return 2;
+    else if (x == '*' || x == '/') return 3;
+    else 
+    else
+        return -1;
 }
 
 int conv(char x) {
-  char mas[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-  for (int i = 0; i < 10; i++) {
-    if (x == mas[i])
-      return i;
-  }
-  return -1;
+    char mas[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    for (int i = 0; i < 10; i++) {
+        if (x == mas[i])
+            return i;
+    }
+    return -1;
 }
 
-
-
 std::string infx2pstfx(std::string inf) {
-  TStack<char>  stack0;
-  std::string str = "";
-  for (int i = 0; i < inf.length(); i++) {
-    if (number(inf[i])) {
-      str += inf[i];
-      str += " ";
-    } else if (inf[i] == '(') {
-      stack0.push(inf[i]);
-    } else if (inf[i] == ')') {
-      while (!stack0.isEmpty() && stack0.get() != '(') {
-        str += stack0.get();
-        str += " ";
-        stack0.pop();
-      }
-      stack0.pop();
-    } else if (operations(inf[i])) {
-      while (!stack0.isEmpty() && priority(stack0.get()) \
-             >= priority(inf[i])) {
-        str += stack0.get();
-        str += " ";
-        stack0.pop();
-      }
-      stack0.push(inf[i]);
+    TStack<char>  stack1;
+    std::string str = "";
+    for (int i = 0; i < inf.length(); i++) {
+        if (num(inf[i])) {
+            str += inf[i];
+            str += " ";
+        } else if (inf[i] == '(') {
+            stack1.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (!stack1.isempty() && stack1.get() != '(') {
+                str += stack1.get();
+                str += " ";
+                stack1.pop();
+            }
+            stack1.pop();
+        } else if (op(inf[i])) {
+            while (!stack1.isempty() && pr(stack1.get()) \
+                   >= pr(inf[i])) {
+                str += stack1.get();
+                str += " ";
+                stack1.pop();
+            }
+            stack1.push(inf[i]);
+        }
     }
-  }
-  while (!stack0.isEmpty()) {
-    str += stack0.get();
-    str += " ";
-    stack0.pop();
-  }
-  str.erase(str.length() - 1, 1);
-  return str;
+    while (!stack1.isempty()) {
+        str += stack1.get();
+        str += " ";
+        stack1.pop();
+    }
+    str.erase(str.length() - 1, 1);
+    return str;
 }
 
 int eval(std::string pref) {
-TStack<int> stack1;
-  for (int i = 0; i < pref.length(); i+=2) {
-    if (number(pref[i])) {
-      stack1.push(conv(pref[i]));
-    } else if (operations(pref[i])) {
-      int b = stack1.get();
-      stack1.pop();
-      int a = stack1.get();
-      stack1.pop();
-      if (pref[i] == '+') {
-        stack1.push(a + b);
-      } else if (pref[i] == '-') {
-        stack1.push(a - b);
-      } else if (pref[i] == '*') {
-        stack1.push(a * b);
-      } else if (pref[i] == '/') {
-        stack1.push(a / b);
-      }
+    TStack<int> stack2;
+    for (int i = 0; i < pref.length(); i+=2) {
+        if (num(pref[i])) {
+            stack2.push(conv(pref[i]));
+        } else if (op(pref[i])) {
+            int b = stack2.get();
+            stack2.pop();
+            int a = stack2.get();
+            stack2.pop();
+            if (pref[i] == '+') {
+                stack2.push(a + b);
+            } else if (pref[i] == '-') {
+                stack2.push(a - b);
+            } else if (pref[i] == '*') {
+                stack2.push(a * b);
+            } else if (pref[i] == '/') {
+                stack2.push(a / b);
+            }
+        }
     }
-  }
-  return stack1.get();
+    return stack2.get();
 }
